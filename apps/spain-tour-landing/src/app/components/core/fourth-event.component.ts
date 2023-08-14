3817294baefd48cb4e6c944d83233e4debe660a0
Andrés Guerrero <andresmgsl@gmail.com>
2023-08-14 14:48:11 +0100

    chore: blue and pink post it images replaced

--- apps/spain-tour-landing/src/app/components/core/fourth-event.component.ts
+++ apps/spain-tour-landing/src/app/components/core/fourth-event.component.ts
@@ -126,7 +126,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <!-- Post-it information -->
         <div class="relative w-[22vw] mt-[2vw] ml-[5vw] -rotate-6 order-1">
           <img
-            src="https://res.cloudinary.com/pwcaguerrero/image/upload/w_422/q_auto/f_auto/v1691707711/images/post_it_blue_sp57sj.webp"
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/w_422/q_auto/f_auto/v1692020336/post_it_blue_1_i3wxu2.png"
             alt="blue post-it with some information"
             class="relative w-full"
           />
