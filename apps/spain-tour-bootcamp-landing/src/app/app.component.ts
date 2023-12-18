9dec8424da1d28165b6657855f2e514744039de3
Andrés Guerrero <andresmgsl@gmail.com>
2023-12-18 18:27:36 +0000

    feat: project created a firebase deploy setted up

---
+++ apps/spain-tour-bootcamp-landing/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bootcamp-root',
+  template: `<bootcamp-nx-welcome></bootcamp-nx-welcome> `,
+  styles: [''],
+})
+export class AppComponent {
+  title = 'spain-tour-post-bootcamp';
+}
