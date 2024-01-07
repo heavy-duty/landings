d9086ab8ed632251caa7feb71a0d4e7eb3b47629
Andrés Guerrero <andresmgsl@gmail.com>
2024-01-07 21:57:06 +0000

    feat: mobile design

--- apps/spain-tour-bootcamp-landing/src/app/mobile/mobile.component..ts
+++ apps/spain-tour-bootcamp-landing/src/app/mobile/mobile.component..ts
@@ -20,16 +20,25 @@ import { Component } from '@angular/core';
         />
       </div>
 
-      <div id="desktop-table" class="absolute -bottom-[9.5vw]">
-        <img 
-        src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1702928477/table_xwe3xs.png"
-        alt="Desktop wood table pixel art"
-        class="w-[99.9vw] h-[60vw]"
-        />
-      </div>
+      <div class="relative flex justify-center mt-[10vw]">
 
-      <div class="relative flex justify-center mt-[10vw] overflow-x-hidden">
-        <div id="computer-speaker-left" class="absolute left-[5vw] bottom-[15vw]">
+        <div id="desktop-table" class="absolute top-[54.1vw]">
+          <img 
+          src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1702928477/table_xwe3xs.png"
+          alt="Desktop wood table pixel art"
+          class="w-[99.9vw] h-[60vw]"
+          />
+        </div>
+
+        <div id="wall-shadow" class="absolute top-[23.4vw]  z-10">
+          <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704135252/shadow_ztxbuz.png"
+              alt="Shadow on the wall pixel art"
+              class="w-[100vw]"
+            />
+        </div> 
+
+        <div id="computer-speaker-left" class="absolute left-[5vw] bottom-[15vw]  z-10">
           <img 
             src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1703956343/Stereo_c3sfcm.png"
             alt="Vintage computer left speaker pixel art"
@@ -37,7 +46,7 @@ import { Component } from '@angular/core';
           />
         </div>  
 
-        <div id="computer-monitor" class="z-10">
+        <div id="computer-monitor" class="z-20 relative mt-[0.4vw]">
           <img 
             src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1702928464/screen_vpremv.png"
             alt="Vintage computer monitor pixel art"
@@ -45,7 +54,7 @@ import { Component } from '@angular/core';
           />
         </div>
 
-        <div id="computer-speaker-right" class="absolute right-[0vw] bottom-[15vw] z-20">
+        <div id="computer-speaker-right" class="absolute right-[0vw] bottom-[15vw] z-30">
           <img 
             src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1703956343/Stereo_c3sfcm.png"
             alt="Vintage computer right speaker pixel art"
@@ -54,12 +63,36 @@ import { Component } from '@angular/core';
         </div>
       </div>
 
-      <div>
+      <div class="relative">
         <div id="computer-keyboard" class="relative mt-[2vw]">
           <img 
             src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1702928472/keyboard_r0owce.png"
             alt="Vintage computer keyboard pixel art"
             class="relative w-[70vw] rotate-3 left-[1vw]"
+          />
+        </div>
+
+        <div id="cup-coffee" class="absolute -top-[17.5vw] right-[1vw] z-30">
+          <img 
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1703957915/Smoke_Dark_hzsnah.gif"
+            alt="Vintage coffee smoke gif pixel art"
+            class="w-[13vw] absolute top-[-6.3vw] right-[0.3vw] opacity-25"
+          />
+        
+          <a href="https://solana.com" target="_blank">
+            <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704136782/Coffee_1_f1esg4.png"
+              alt="Vintage solana cup of coffee pixel art"
+              class="w-[13vw]"
+            />
+          </a>  
+        </div>
+
+        <div id="ashtray" class="absolute -top-[10.5vw] left-[4vw]">
+          <img 
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704465247/Ashtray_2_y7wyub.png"
+            alt="Vintage ashtray pixel art"
+            class="w-[12vw]"
           />
         </div>
 
@@ -82,6 +115,119 @@ import { Component } from '@angular/core';
           />
         </div>
       </div>
+
+      <div class="relative mt-[10vw]">
+        <div id="bookshelf" class="relative -left-[37vw] z-30">
+          <img 
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704138687/Books_Shell_1_q13cdi.png"
+            alt="Vintage bookshelf pixel art"
+            class="w-[100vw]"
+          />
+        </div>
+
+
+        <div id="bookshelf-books-up" class="absolute top-[6vw] left-[7vw] w-[40vw] h-[12.5vw] z-40">
+          <a href="https://www.soldev.app/" target="_blank" >
+            <img  
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704289333/Blue_Book_qogwrt.png"
+              alt="Vintage blue book pixel art"
+              class="w-[7.3vw] absolute z-10 top-[0vw]"
+            />
+          </a>
+          <a href="https://www.soldev.app/" target="_blank" >
+            <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704289333/Red_Book_ui9oqa.png"
+              alt="Vintage red book pixel art"
+              class="w-[6.7vw] absolute z-10 left-[4.5vw] top-[1.8vw]"
+            />
+          </a>
+          <a href="https://www.soldev.app/" target="_blank" >
+            <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704289332/Green_Book_wfxtwg.png"
+              alt="Vintage green book pixel art"
+              class="w-[7.1vw] absolute z-10 left-[9vw] top-[0.3vw]"
+            />
+          </a>
+          <a href="https://www.soldev.app/" target="_blank" >
+            <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704289332/Pink_Book_wuxdpf.png"
+              alt="Vintage green book pixel art"
+              class="w-[7vw] absolute z-10 left-[13.5vw] top-[0.7vw]"
+            />        
+          </a>
+        </div>
+
+        <div id="bookshelf-books-down" class="absolute bottom-[0vw] left-[7vw] w-[6vw] h-[27vw] z-[30]">
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Yellow_Simple_Book__xl7bko.png"
+            alt="Vintage yellow book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] -left-[1.1vw]"
+          />  
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/White_Simple_Book__qqytug.png"
+            alt="Vintage white book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[3.6vw]"
+          />
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Blue_Simple_Book__foovlc.png"
+            alt="Vintage blue book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[8.6vw]"
+          />
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Green_Simple_Book_lcseet.png"
+            alt="Vintage green book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[13vw]"
+          />
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Purple_Simple_Book_apk2zp.png"
+            alt="Vintage purple book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[17.5vw]"
+          />
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Yellow_Simple_Book__xl7bko.png"
+            alt="Vintage yellow book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[22vw]"
+          />  
+          <img  
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704402554/Red_Simple_Book__fwzsq5.png"
+            alt="Vintage red book pixel art"
+            class="w-[4.9vw] absolute z-10 top-[0vw] left-[26.5vw]"
+          />
+        </div>
+
+        <div id="wall-shadow-desktop" class="absolute w-[100vw] h-[70vw] bg-black -top-[7.2vw] opacity-30"></div>
+
+        <div id="wall-shadow-room" class="absolute bottom-[8vw] -right-[3vw]  z-10">
+          <img 
+              src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704135252/shadow_ztxbuz.png"
+              alt="Shadow on the wall pixel art"
+              class="w-[100vw]"
+            />
+        </div> 
+
+        <div id="trash-can" class="absolute z-10 -right-[2vw] bottom-0">
+          <img 
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704654302/Trash_Can_vjrqps.png"
+            alt="Trash can pixel art"
+            class="w-[25vw]"
+          />
+        </div>
+        
+
+        <div id="room-floor" class="absolute w-[100vw] bg-[#faebd7] h-[8.5vw] bottom-[0vw]"></div>
+      </div>
+
+      <div id="funko-builder" class="absolute bottom-[32vw] left-[28.5vw] z-[40]">
+        <a href="https://heavyduty.builders" target="_blank">
+          <img 
+            src="https://res.cloudinary.com/pwcaguerrero/image/upload/q_auto/f_auto/v1704138873/Builder_l1fch8.png"
+            alt="Vintage funko builder pixel art"
+            class="w-[15vw]"
+          />
+        </a>
+      </div>
+
+
     </div>
   `,
   standalone: true,
