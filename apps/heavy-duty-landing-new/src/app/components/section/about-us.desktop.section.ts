652b1bcc36d614580e51d0c49ec58755c697a53e
Andres <andresmgsl@gmail.com>
2024-05-08 03:23:33 +0100

    feat: design added to about and hero

--- apps/heavy-duty-landing-new/src/app/components/section/about-us.desktop.section.ts
+++ apps/heavy-duty-landing-new/src/app/components/section/about-us.desktop.section.ts
@@ -4,8 +4,31 @@ import { Component } from '@angular/core';
   imports: [],
   selector: 'hdb-about-us-desktop',
   template: `
-    {{text}}
-  
+    <section class="relative py-[1.3vw] px-[3vw] mb-[12vw]">
+      <img src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1715130050/Bricks_Bricks_Section_1_swvdm2.png" alt="Bloques completos" class="absolute z-10 w-[90vw] right-[5vw]"/>
+
+      <div class="w-full flex mt-[4.35vw] justify-center">
+        <img src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1715131417/small_sign_z6cbac.png" class="relative w-[33vw] z-10" alt="Letrero" />
+      </div>
+
+      <div class="relative flex gap-1 mt-[25vw]">
+        <div class="block relative ml-[4vw]">
+          <img src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1715133022/toolbox_shadow_rik3fo.png" class="relative w-[7vw] z-10 top-[6.3vw]" alt="Caja de herramienta" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1715133022/new_bob_shadow_nffjmi.png" class="relative w-[9vw] z-10 top-[0.8vw]" alt="Bob pequeño" />
+        </div>
+
+        <div class="absolute z-10 right-0 -top-[15vw]">
+          <img src="https://res.cloudinary.com/andresmgsl/image/upload/v1715132871/bob_dialog_desktop_vkedge.png" class="relative w-[71.7vw]" alt="Globo de chat de bob"/>
+        </div>
+
+        <div class="ml-[60vw]">
+          <img src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1715133022/cat_shadow_u7em8w.png" class="relative w-[9vw] top-[10vw] right-0 z-10" alt="Bob pequeño" />
+        </div>
+      </div>
+      <div class="gradient absolute w-full h-[10vw] left-0 -bottom-[0.8vw]"></div>
+    </section>
     `,
   styles: ``,
 })
--- apps/heavy-duty-landing-new/src/app/components/section/hero.desktop.section.ts
+++ apps/heavy-duty-landing-new/src/app/components/section/hero.desktop.section.ts
@@ -4,12 +4,19 @@ import { Component } from '@angular/core';
   imports: [],
   selector: 'hdb-hero-desktop',
   template: `
-    <section class="py-[1.3vw] px-[3vw]">
-      <h1 class="text-[3.5vw] mb-[1.5vw]">HEAVY DUTY <br/> BUILDERS<br/> COMMUNITY<br/></h1>
+    <section class="relative py-[1.3vw] px-[3vw] mb-[12vw]">
+      <h1 class="text-[4vw] mb-[1.5vw]">HEAVY DUTY <br/> BUILDERS<br/> COMMUNITY<br/></h1>
       <p class="text-[1vw] mb-[1.5vw]">¡Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit!</p>
-      <button class=" bg-pixel-purple py-[0.4vw] px-[1.2vw] border-[0.2vw] border-black text-white text-[1.5vw]">JOIN THE BOOT CAMP</button>
+      <button class=" bg-pixel-purple py-[0.4vw] px-[2.2vw] border-[0.2vw] border-black text-white text-[2vw]">JOIN THE BOOT CAMP</button>
+
+      <img src="https://res.cloudinary.com/andresmgsl/image/upload/w_600/q_auto/f_auto/v1714505817/Bricks_Blocks_Hero_jagbqi.png" alt="Bloques a la mitad" class="absolute w-[48vw] top-[1vw] right-[3vw]" />
+
+      <img src="https://res.cloudinary.com/andresmgsl/image/upload/w_500/q_auto/f_auto/v1714505817/new_bob_shadow_nffjmi.png" alt="Bob" class="absolute z-10 w-[16vw] top-[12vw] right-[24vw]" />
+
+      <img src="https://res.cloudinary.com/andresmgsl/image/upload/w_500/q_auto/f_auto/v1714505817/new_alice_shadow_c7nff5.png" alt="Alice" class="absolute z-10 w-[16vw] top-[12vw] right-[6vw]" />
+
     </section>
-  
+    <div class="gradient absolute w-full h-[10vw] left-0 -bottom-[3vw]"></div>
     `,
   styles: ``,
 })
