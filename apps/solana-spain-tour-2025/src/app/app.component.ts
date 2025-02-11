a72ef0fcea0ab23e3be69818f46bfb9155e67609
Andres Guerrero <andresmgsl@gmail.com>
2025-02-11 17:44:21 +0000

    chore: removing old events

--- apps/solana-spain-tour-2025/src/app/app.component.ts
+++ apps/solana-spain-tour-2025/src/app/app.component.ts
@@ -54,72 +54,8 @@ import { FirstEventComponent } from './components/core/first-event.component';
 
     <!-- Events section -->
     <section class="relative px-[7vw] overflow-hidden pb-[5vw]">
-      <!-- third event -->
-      <spain-tour-third-event></spain-tour-third-event>
-
-      <!-- Orange zigzag divider -->
-      <div class="absolute w-full flex left-0 z-10">
-        <div class="absolute w-[70vw] -top-[3vw] -rotate-3">
-          <img
-            src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1691707714/images/zigzag_orange_tqiwna.webp"
-            class="w-full"
-            alt="Orange zigzag divider "
-          />
-        </div>
-      </div>
-
-      <!-- line from third event to fourth event-->
-      <div class="relative w-full mb-[16vw]">
-        <img
-          src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1691707714/images/arrow_3-4_hbjenp.webp"
-          class="absolute w-[55vw] -right-[4vw] -top-[37vw]"
-          alt="arrow from third postcard event to fourth postcard event"
-        />
-      </div>
-
-      <!-- train icon -->
-      <div class="absolute w-full mb-[16vw]">
-        <img
-          src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1692022453/train_a0d2vt.webp"
-          class="absolute w-[16vw] right-[18vw] -top-[22vw] -rotate-[320deg]"
-          alt="train sticker"
-        />
-      </div>
-
-      <!-- second event-->
-      <spain-tour-second-event></spain-tour-second-event>
-
-      <!-- Green zigzag divider -->
-      <div class="absolute w-full flex left-0 z-10">
-        <div class="absolute w-[70vw] top-[3vw] -right-[1vw] -rotate-6">
-          <img
-            src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1691707713/images/zigzag_green_xtvnyq.webp"
-            class="w-full"
-            alt="Green zigzag divider "
-          />
-        </div>
-      </div>
-
-      <!-- line from second event to third event-->
-      <div class="relative w-full mb-[16vw]">
-        <img
-          src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1691707711/images/arrow_2-3_pczd47.webp"
-          class="absolute w-[50vw] -left-[8vw] -top-[28vw]"
-          alt="arrow from second postcard event to third postcard event"
-        />
-      </div>
-
-      <!-- train icon -->
-      <div class="absolute w-full mb-[16vw]">
-        <img
-          src="https://res.cloudinary.com/andresmgsl/image/upload/q_auto/f_auto/v1692022453/train_a0d2vt.webp"
-          class="absolute w-[16vw] left-[6vw] -top-[12vw] rotate-12 scale-x-[-1]"
-          alt="train sticker"
-        />
-      </div>
-
-      <!-- third event-->
-      <spain-tour-first-event></spain-tour-first-event>
+      <!-- Working here -->
+       
     </section>
 
     <!-- Divider & Doubts text -->
